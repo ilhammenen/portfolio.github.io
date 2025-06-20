af9eb3070a0587ae83bea4807ecf16caf7c49174
ilhammenen <155568646+ilhammenen@users.noreply.github.com>
2025-06-20 17:58:21 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base:'/portfolio.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
